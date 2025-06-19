d97cb0e4a2c590abf751740cff32cde989b0ac97
IsmailZhaf <ismailzhanfeari@gmail.com>
2025-06-19 12:42:53 +0700

    fix: fixing layout

--- app/request/sludge-pickup/page.tsx
+++ app/request/sludge-pickup/page.tsx
@@ -5,15 +5,15 @@ import PickupForm from "@/components/pickup-form";
 
 export default function Page() {
     return (
-        <div className="flex justify-evenly py-20 w-screen">
+        <div className="flex justify-evenly py-8 w-screen">
             <ClientNavbar />
-            <div className="w-[35%] space-y-7">
+            <div className="w-[35%] space-y-3">
                 <h1 className="text-5xl font-radley">Pick Up Your Sludge Today</h1>
                 <p className="text-[#505050] text-lg">We manage both B3 and non-B3 sludge responsibly using a zero-waste approach delivered through our licensed partnership with Pituku.</p>
                 <PickupForm />
             </div>
             <div>
-                <Image src={"/pickup.svg"} alt={"pickup"} width={511} height={744} />
+                <Image src={"/pickup.svg"} alt={"pickup"} width={511} height={544} />
             </div>
         </div>
     );
