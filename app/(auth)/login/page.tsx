d568ec990ecacb9a58b0a5786c0e17f8682918c6
IsmailZhaf <ismailzhanfeari@gmail.com>
2025-06-19 11:03:55 +0700

    fix: fixing input border style

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -56,8 +56,7 @@ export default function Page() {
                                 value={loginData.email}
                                 onChange={(e) => setLoginData({ ...loginData, email: e.target.value })}
                                 type="email"
-                                placeholder="Enter your email"
-                                className="w-full p-4 border border-gray-200 rounded-xl focus:ring-2 focus:ring-black focus:border-transparent"
+                                className="w-full p-2 border border-primary rounded-md focus:outline-none"
                                 required
                             />
                         </div>
@@ -66,11 +65,10 @@ export default function Page() {
                             <label className="block text-sm font-semibold text-gray-900 mb-2">Password</label>
                             <div className="relative">
                                 <input
-                                    placeholder="Enter your password"
                                     value={loginData.password}
                                     onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
                                     type={showPassword ? "text" : "password"}
-                                    className="w-full p-4 border border-gray-200 rounded-xl focus:ring-2 focus:ring-primary focus:border-transparent"
+                                    className="w-full p-2 border border-primary rounded-md focus:outline-none"
                                     required
                                 />
                                 <button type="button" onClick={() => setShowPassword(!showPassword)} className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600">
@@ -83,7 +81,7 @@ export default function Page() {
                             Forgot password?
                         </Link>
 
-                        <button type="submit" className="w-full bg-primary mt-6 text-white py-3 px-6 rounded-xl ">
+                        <button type="submit" className="w-full bg-primary mt-6 text-white py-2 px-6 rounded-md ">
                             Sign In
                         </button>
                     </form>
@@ -92,7 +90,7 @@ export default function Page() {
                         <span className="px-4 text-sm text-gray-500">or</span>
                         <div className="flex-1 border-t border-primary"></div>
                     </div>
-                    <button className="w-full bg-primary border border-gray-200 text-secondary py-3 px-6 rounded-xl  flex items-center justify-center">
+                    <button className="w-full bg-primary border border-primary text-secondary py-2 px-6 rounded-md  flex items-center justify-center">
                         <svg className="w-5 h-5 mr-3" viewBox="0 0 24 24">
                             <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
                             <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
