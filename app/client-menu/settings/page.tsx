6d36ccae2cfc328ffdcf2f213fc735b5a739625b
Eldovyn <farras.pramudita@gmail.com>
2025-07-11 14:28:37 +0700

    fix

--- app/client-menu/settings/page.tsx
+++ app/client-menu/settings/page.tsx
@@ -70,7 +70,6 @@ export default function Page() {
         setFormErrorsCompanyInformation(errors);
     }
 
-
     const { mutate } = useMutation({
         mutationFn: async ({
             formData1,
