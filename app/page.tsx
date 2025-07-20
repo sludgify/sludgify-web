2510d18c4f21ffd848bcf2fa541b9b9f92aedc10
IsmailZhaf <ismailzhanfeari@gmail.com>
2025-07-20 18:06:16 +0700

    feat: add mobile version of landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import { BannerFooter } from "@/components/banner-footer";
+
+import { ValuesCarousel } from "@/components/values-carousel";
 
 export default function Home() {
     const content = [
@@ -58,93 +61,108 @@ export default function Home() {
             url: "service4.svg",
         },
     ];
+
+    const values = [
+        {
+            id: 1,
+            title: "Transparancy",
+            description: "We transform sustainability performance through integrated dashboards and verifiable data systems that enable data-driven decisions, build stakeholder confidence, and deliver measurable impact",
+            className: "border border-[#525252] bg-white text-[#525252]",
+        },
+        {
+            id: 2,
+            title: "Sustainability",
+            description: "We enable zero-waste sludge operations through comprehensive B3 and non-B3 management that creates competitive advantage while ensuring regulatory compliance and sustainable growth",
+            className: "bg-primary text-secondary border border-[#525252]",
+        },
+        {
+            id: 3,
+            title: "Collaboration",
+            description: "Through our strategic partnership with licensed operator Pituku, we deliver compliant, risk-mitigated sludge management that creates measurable stakeholder value",
+            className: "border border-[#525252] bg-white text-[#525252]",
+        },
+    ];
     return (
         <div className="flex min-h-screen w-full flex-col overflow-hidden">
             <Navbar />
             <main className="flex-1 w-full overflow-hidden">
                 {/* Hero Section*/}
-                <div className="bg-[url('/bg-about.svg')] text-secondary w-full bg-no-repeat bg-cover bg-center h-[690px] space-y-3 flex flex-col justify-center gap-3 p-20 ">
-                    <h1 className="text-6xl font-radley">One Platform for Waste Compliance, ESG Reporting, and Carbon Reduction</h1>
-                    <p className="max-w-[1000px]">
+                <div className="bg-[url('/bg-about.svg')] text-secondary w-full bg-no-repeat bg-cover bg-center h-[300px] md:h-[690px] md:space-y-3 flex flex-col justify-center gap-3 p-8 md:p-20 ">
+                    <h1 className="text-2xl md:text-6xl font-radley">One Platform for Waste Compliance, ESG Reporting, and Carbon Reduction</h1>
+                    <p className="max-w-[1000px] text-[10px] md:text-base">
                         Sludgify offers an integrated solution that transforms industrial waste management into a strategic ESG and decarbonization asset. Our platform ensures regulatory compliance, delivers audit-ready sustainability
                         reports, and quantifies carbon impact, enabling companies to meet environmental goals with full transparency and measurable results
                     </p>
-                    <div className="flex items-center gap-2 h-14">
+                    <div className="flex items-center md:gap-2 md:h-14">
                         <div className="flex flex-row -space-x-2">
-                            <Image src="Ellipse 2.svg" alt="Ellipse 2" width={50} height={50} />
-                            <Image src="Ellipse 3.svg" alt="Ellipse 3" width={50} height={50} />
-                            <p className="bg-gradient-to-b from-primary to-[#525252] h-14 w-14 rounded-full flex items-center justify-center text-secondary font-radley">50+</p>
+                            <Image src="Ellipse 2.svg" alt="Ellipse 2" width={50} height={50} className="w-[38px] h-[38px] md:w-[50px] md:h-[50px]" />
+                            <Image src="Ellipse 3.svg" alt="Ellipse 3" width={50} height={50} className="w-[38px] h-[38px] md:w-[50px] md:h-[50px]" />
+                            <p className="bg-gradient-to-b from-primary to-[#525252] w-[38px] h-[38px] md:w-[50px] md:h-[50px] rounded-full flex items-center justify-center text-secondary font-radley">50+</p>
                         </div>
-                        <Separator orientation="vertical" className="w-[10px] h-full bg-white mx-4" />
-                        <Button className="font-radley p-6 text-lg cursor-pointer bg-gradient-to-b from-primary to-[#525252]">Join Us</Button>
+                        <Separator orientation="vertical" className="w-[15px] h-full bg-white mx-4" />
+                        <Button className="font-radley md:p-6 p-3 text-sm md:text-lg cursor-pointer bg-gradient-to-b from-primary to-[#525252]">Join Us</Button>
                     </div>
                 </div>
                 {/* Content Section */}
-                <div className="container flex justify-center gap-8 px-10 mx-auto py-10">
-                    <Image src={"/image-about.svg"} width={448.35} height={472} alt="Sludgify illustration" />
-                    <div className="max-w-[700px] flex flex-col space-y-6 justify-center">
-                        <h1 className="text-4xl font-radley">We transform your ESG strategy into quantifiable business value and competitive advantage</h1>
-                        <p className="text-lg text-[#505050]">
+                <div className="container flex flex-col-reverse md:flex-row justify-center gap-8 md:px-10 px-8 mx-auto py-5 md:py-10">
+                    <Image src={"/image-about.svg"} width={448.35} height={472} alt="Sludgify illustration" className="hidden md:block" />
+                    <Image src={"/p (4).svg"} width={334} height={162} alt="Sludgify illustration" className="md:hidden block" />
+                    <div className="max-w-[700px] flex flex-col space-y-3 md:space-y-6 justify-center">
+                        <h1 className="text-sm md:text-4xl font-radley">We transform your ESG strategy into quantifiable business value and competitive advantage</h1>
+                        <p className="text-[10px] md:text-lg text-[#505050] font-calibri">
                             Sludgify accelerates enterprise sustainability transformation through integrated ESG solutions that deliver measurable impact. We enable organizations to achieve operational excellence across waste-to-value
                             conversion, carbon strategy, regulatory compliance, and AI-driven analytics, creating competitive advantage while meeting stakeholder expectations for transparent sustainability leadership.
                         </p>
-                        <div className="grid grid-cols-2 items-center gap-8 my-6">
+                        <div className="grid grid-cols-4 md:grid-cols-2 md:items-center gap-8 md:my-6">
                             {content.map((item, index) => (
                                 <div key={index}>
-                                    <h1 className="text-3xl font-radley">{item.h1}</h1>
-                                    <h2 className="text-lg font-radley text-[#000000]">{item.h2}</h2>
+                                    <h1 className="text-sm md:text-3xl font-radley">{item.h1}</h1>
+                                    <h2 className="text-[8px] md:text-lg font-radley text-[#000000]">{item.h2}</h2>
                                 </div>
                             ))}
                         </div>
                     </div>
                 </div>
                 {/* Core Value Section */}
-                <div className="bg-[#FAFAFA] font-radley  p-12 flex items-start justify-evenly gap-8">
+                <div className="bg-[#FAFAFA] font-radley md:p-12 md:flex items-start justify-evenly md:gap-8">
                     {value.map((item, index) => (
-                        <div key={index} className="flex flex-col gap-4 text-balance px-12 text-lg mb-8 w-[750px]">
-                            <div className="flex items-center gap-4">
-                                <div className="w-3 h-3 bg-[#505050]" />
-                                <h1 className="text-3xl">{item.title}</h1>
+                        <div key={index} className="flex flex-col gap-1 md:gap-4 text-balance px-8 md:px-12 text-lg mb-8 md:w-[750px] w-[334px]">
+                            <div className="flex items-center gap-2 md:gap-4">
+                                <div className="w-1.5 h-1.5 md:w-3 md:h-3 bg-[#505050]" />
+                                <h1 className="text-sm md:text-3xl">{item.title}</h1>
                             </div>
-                            <p>{item.desc}</p>
+                            <p className="md:text-base text-[10px] text-[#525252] font-calibri">{item.desc}</p>
                         </div>
                     ))}
                 </div>
                 {/* Core Value Section */}
-                <div className=" w-screen bg-white p-12 flex flex-col items-center justify-evenly gap-8">
-                    <h1 className="text-6xl font-radley text-primary">What Drives Us</h1>
-                    <p className="text-[#525252] text-lg max-w-[1000px] text-center">
-                        KelolaCarbon is built on the belief that waste compliance, ESG reporting, and carbon reduction must be integrated. Our core value is enabling businesses to turn environmental obligations into measurable, strategic
-                        climate impact, efficiently and at scale
+                <div className=" w-screen bg-white p-8 md:p-12 flex flex-col items-center justify-evenly gap-2 md:gap-8">
+                    <h1 className="text-sm md:text-6xl font-radley text-primary">What Drives Us</h1>
+                    <p className="text-[#525252] text-[10px] md:text-lg max-w-[1000px] text-center">
+                        Sludgify is built on the belief that waste compliance, ESG reporting, and carbon reduction must be integrated. Our core value is enabling businesses to turn environmental obligations into measurable, strategic
+                        climate impact, efficiently and at scale.
                     </p>
-                    <div className="flex gap-5">
-                        <div className="border border-[#525252] rounded-md w-[428px] p-6 space-y-5 h-[365px] drop-shadow-md">
-                            <h1 className="text-4xl font-radley">Transparancy</h1>
-                            <p className="text-lg text-[#525252]">
-                                We transform sustainability performance through integrated dashboards and verifiable data systems that enable data-driven decisions, build stakeholder confidence, and deliver measurable impact
-                            </p>
-                        </div>
-                        <div className="bg-primary text-secondary border border-[#525252] rounded-md w-[428px] p-6 space-y-5 h-[365px] drop-shadow-md">
-                            <h1 className="text-4xl font-radley">Sustainability</h1>
-                            <p className="text-lg">We enable zero-waste sludge operations through comprehensive B3 and non-B3 management that creates competitive advantage while ensuring regulatory compliance and sustainable growth</p>
-                        </div>
-                        <div className="border border-[#525252] rounded-md w-[428px] p-6 space-y-5 h-[365px] drop-shadow-md">
-                            <h1 className="text-4xl font-radley">Collaboration</h1>
-                            <p className="text-lg text-[#525252]">Through our strategic partnership with licensed operator Pituku, we deliver compliant, risk-mitigated sludge management that creates measurable stakeholder value</p>
-                        </div>
+                    <div className="hidden md:flex gap-5">
+                        {values.map((item) => (
+                            <div key={item.id} className={`rounded-md drop-shadow-md w-full max-w-[428px] h-[365px] px-3 py-3 ${item.className}`}>
+                                <h1 className="text-4xl font-radley">{item.title}</h1>
+                                <p className="text-lg mt-5">{item.description}</p>
+                            </div>
+                        ))}
                     </div>
+                    {/* Mobile Values Carousel */}
+                    <ValuesCarousel values={values} />
                 </div>
                 {/* Services Section */}
                 <div className="container flex flex-col justify-center items-center mx-auto px-10 py-20">
-                    <h1 className="text-6xl font-radley text-center mb-12">Our Services</h1>
-
+                    <h1 className="text-[20px] md:text-6xl font-radley text-center mb-5 md:mb-12">Our Services</h1>
                     <div className="w-full">
-                        <div className="columns-1 md:columns-2 gap-8 max-w-[1248px] mx-auto">
+                        <div className="columns-1 md:columns-2 gap-3 md:gap-8 max-w-[1248px] mx-auto">
                             {services.map((service, index) => (
-                                <div key={index} className="mb-8 break-inside-avoid bg-white p-6 w-[592px] inline-block">
-                                    <Image src={service.url} alt={service.title} width={592} height={400} className="mb-4 object-cover" />
-                                    <h2 className="text-4xl font-radley mb-4">{service.title}</h2>
-                                    <p className="text-xl text-[#505050]">{service.desc}</p>
+                                <div key={index} className="mb-4 md:mb-8 break-inside-avoid bg-white md:p-6 w-[325px] md:w-[592px] inline-block">
+                                    <Image src={service.url} alt={service.title} width={592} height={400} className="mb-1 md:mb-4 rounded-[8px] object-cover" />
+                                    <h2 className="text-xl md:text-4xl font-radley mb-1 md:mb-4">{service.title}</h2>
+                                    <p className="text-[10px] md:text-xl text-[#525252]">{service.desc}</p>
                                 </div>
                             ))}
                         </div>
--- components/banner-footer.tsx
+++ components/banner-footer.tsx
@@ -3,13 +3,13 @@ import { Button } from "./ui/button";
 
 export const BannerFooter = () => {
     return (
-        <div className="relative container rounded-2xl bg-[url('/banner.jpg')] bg-no-repeat bg-cover bg-center w-[1000px] h-[376px] mx-auto px-10 flex flex-col items-start justify-center gap-6 overflow-hidden -mb-20 z-10">
+        <div className="relative container rounded-2xl bg-[url('/banner.jpg')] bg-no-repeat bg-cover bg-center w-[335px] md:w-[1000px] h-[128px] md:h-[376px] mx-auto px-10 flex flex-col items-start justify-center gap-6 overflow-hidden -mb-20 z-10">
             {/* Overlay gradient */}
             <div className="absolute inset-0 bg-gradient-to-r from-black to-transparent z-0 rounded-2xl" />
 
-            <div className="relative z-10 text-white text-left font-radley w-[522px]">
-                <h1 className="text-5xl">Take Your First Step in ESG with Sludgify</h1>
-                <Button className="mt-6 bg-[#1B2F73] text-secondary font-radley text-2xl p-6 cursor-pointer">Let’s Work Together</Button>
+            <div className="relative z-10 text-white text-left font-radley md:w-[522px]">
+                <h1 className="text-xl md:text-5xl">Take Your First Step in ESG with Sludgify</h1>
+                <Button className="md:mt-6 mt-3 bg-[#1B2F73] text-secondary font-radley text-sm md:text-2xl md:p-6 p-3 cursor-pointer">Let’s Work Together</Button>
             </div>
         </div>
     );
